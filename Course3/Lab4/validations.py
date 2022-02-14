76881aedc48af5e2cd30528179e1ad90a787f430
agyago <42577978+agyago@users.noreply.github.com>
2022-02-14 18:09:26 +0800

    adding Results

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -15,7 +15,7 @@ def validate_user(username, minlen):
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
-    # Usernames can't begin with a number, underscores
+    # Usernames can't begin with a number, underscores, and period
     if re.match('^[0-9_.].*', username):
         return False
     # Usernames can't begin with a number
@@ -24,9 +24,9 @@ def validate_user(username, minlen):
     return True
 
 
-print(validate_user('blue.kale',3))
-print(validate_user('.blue.kale',3))
-print(validate_user('red_quinoa',4))
-print(validate_user('_red_quinoa',4))
-print(validate_user('blue*kale', 3))
-print(validate_user('blue.kale;',3))
+print(validate_user('blue.kale',3))  # True
+print(validate_user('.blue.kale',3)) # False
+print(validate_user('red_quinoa',4)) # True
+print(validate_user('_red_quinoa',4)) # False
+print(validate_user('blue*kale', 3)) # False
+print(validate_user('blue.kale;',3)) # False
